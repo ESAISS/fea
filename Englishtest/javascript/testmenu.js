782fda0a30b38d50b76fbfae793270317bf483b0
ESAISS <117594997+ESAISS@users.noreply.github.com>
2023-02-18 19:46:05 +0100

    Add files via upload

---
+++ Englishtest/javascript/testmenu.js
@@ -0,0 +1,21 @@
+function Testmenu(){
+window.location.replace("testmenu.html");
+}
+function Easytest() {
+window.location = "easytest.html"
+}
+function Mediumtest() {
+window.location = "mediumtest.html"
+}
+function Hardtest() {
+window.location = "hardtest.html"
+}
+function Mixedtest() {
+window.location = "mixedtest.html"
+}
+function Home() {
+	window.location = "index.html"
+}
+function Menu() {
+	window.location = "testmenu.html"
+}
